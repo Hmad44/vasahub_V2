7025c532c9e845080708f81b73150d5c9010ccc2
Ahmed Moumad <133714300+Hmad44@users.noreply.github.com>
2024-04-24 19:16:44 -0500

    fix deploy issues

--- app/lib/auth.js
+++ app/lib/auth.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import bcrypt from 'bcrypt'
+import prisma from "@/lib/prisma"
 import { authConfig } from "./auth.config";
 
 const login = async (credentials) => {
